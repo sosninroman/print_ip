69e5b8da0a7d8245d83634be5ace58e9ab417c4c
sosnin_roman <sosnin_roman@rambler.ru>
2018-12-01 23:41:17 +0500

    test fix

--- main.cpp
+++ main.cpp
@@ -10,15 +10,15 @@ int main()
     std::cout << std::endl;
     print_ip((long long)(8875824491850138409));
     std::cout << std::endl;
-    std::cout << "string" << std::endl;
+    std::cout << "string(192.12.12.12) -> ";
     std::string str("192.12.12.12");
     print_ip(str);
     std::cout << std::endl;
-    std::cout << "vector<int>{2,5,7,3} ->";
+    std::cout << "vector<int>{2,5,7,3} -> ";
     std::vector<int> vint{2,5,7,3};
     print_ip(vint);
     std::cout << std::endl;
-    std::cout << "list<unsigned char>{1,192,0,13} ->";
+    std::cout << "list<unsigned char>{1,192,0,13} -> ";
     std::list<unsigned char> lch{1,192,0,13};
     print_ip(lch);
     std::cout << std::endl;
